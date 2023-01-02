f01faa0a231320824be873d608a44ad761d4b488
HDNM <hoangducnhatminh.dev@gmail.com>
2023-01-03 02:18:44 +0700

    gio hang update

--- src/route/productRouter.js
+++ src/route/productRouter.js
@@ -7,17 +7,10 @@ router.get('/', (req, res, next) => {
     if ((req.query.category === null) || isNaN(req.query.category)) {
         req.query.category = 0;
     }
-<<<<<<< HEAD
-
     if (req.query.search == null || (req.query.search.trim() == '')) {
         req.query.search = '';
     }
 
-=======
-    if(req.query.search == null || (req.query.search.trim() == '')){
-        req.query.search = ''; 
-    }
->>>>>>> ffdd7c9cea9cdf124a7fc2ed24786c8bc35e6c00
     let categoryController = require('../controllers/categoryControllers');
     categoryController
         .getAll()
