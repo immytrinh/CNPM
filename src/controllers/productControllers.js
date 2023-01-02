9e45baa7b52ce220af743197c10b7cbb37948ce3
Trung <trung22112002@gmail.com>
2023-01-03 02:38:03 +0700

    view product detail

--- src/controllers/productControllers.js
+++ src/controllers/productControllers.js
@@ -2,14 +2,30 @@ import db from '../models'
 let controller = {}
 let Product = db.Product
 
-controller.getAll = () => {
-    return new Promise((resolve, reject) => {
+controller.getAll = () =>
+{
+    return new Promise((resolve, reject) =>
+    {
         Product
             .findAll({
                 // include: [{ db: db.Category }],
                 attributes: ['productId', 'name', 'price', 'imagePath', 'description']
             })
             .then(data => resolve(data))
+            .catch(error => reject(new Error(error)))
+    })
+}
+
+controller.getProductById = (id) =>
+{
+    return new Promise((resolve, reject) =>
+    {
+        Product
+            .findAll({
+                attributes: ['productId', 'name', 'price', 'imagePath', 'description'],
+                where: { productID: id }
+            })
+            .then(data => resolve(data[0].dataValues))
             .catch(error => reject(new Error(error)))
     })
 }
--- src/route/productRouter.js
+++ src/route/productRouter.js
@@ -3,20 +3,35 @@ import express from "express";
 let router = express.Router();
 
 
-router.get('/', (req, res, next) => {
+router.get('/', (req, res, next) =>
+{
     let categoryController = require('../controllers/categoryControllers');
     categoryController
         .getAll()
-        .then(data => {
+        .then(data =>
+        {
             res.locals.categories = data;
             let productController = require("../controllers/productControllers")
             return productController.getAll();
         })
-        .then(data => {
+        .then(data =>
+        {
             res.locals.products = data;
             return res.render('category.ejs')
         })
         .catch(error => next(error));
 })
+
+router.get('/:productID', function (req, res, next)
+{
+    let productController = require("../controllers/productControllers")
+    productController
+        .getProductById(req.params.productID)
+        .then(data =>
+        {
+            res.locals.product = data;
+            return res.render('product-detail.ejs')
+        })
+});
 
 module.exports = router;
