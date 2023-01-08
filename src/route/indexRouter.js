daab46bf1be2b3803ec197a3f6669f6abcb1f3f8
Trung <trung22112002@gmail.com>
2023-01-08 13:50:58 +0700

    nothing

--- src/route/indexRouter.js
+++ src/route/indexRouter.js
@@ -3,14 +3,15 @@ import express from "express";
 let router = express.Router();
 
 router.get("/", (req, res, next) => {
-    let categoryController = require("../controllers/categoryControllers");
-    categoryController
-        .getAll()
-        .then(data => {
-            res.locals.categories = data;
-            res.render("index")
-        })
-        .catch(error => next(error));
+    // let categoryController = require("../controllers/categoryControllers");
+    // categoryController
+    //     .getAll()
+    //     .then(data => {
+    //         res.locals.categories = data;
+    //         res.render("index")
+    //     })
+    //     .catch(error => next(error));
+    res.redirect('/products')
 })
 
 module.exports = router;
