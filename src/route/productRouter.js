6f91cac46421c3f695620ee1df40a4849da7f37c
HDNM <hoangducnhatminh.dev@gmail.com>
2023-01-09 12:48:52 +0700

    update

--- src/route/productRouter.js
+++ src/route/productRouter.js
@@ -5,8 +5,7 @@ let router = express.Router();
 
 
 
-router.get('/', (req, res, next) =>
-{
+router.get('/', (req, res, next) => {
     if ((req.query.category === null) || isNaN(req.query.category)) {
         req.query.category = 0;
     }
@@ -16,41 +15,36 @@ router.get('/', (req, res, next) =>
     let categoryController = require('../controllers/categoryControllers');
     categoryController
         .getAll()
-        .then(data =>
-        {
+        .then(data => {
             res.locals.categories = data;
             let productController = require("../controllers/productControllers")
             return productController.getAll(req.query);
         })
-        .then(data =>
-        {
+        .then(data => {
             res.locals.products = data;
             return res.render('category.ejs')
         })
         .catch(error => next(error));
 })
 
-router.get('/:productID', function (req, res, next)
-{
+router.get('/:productID', function (req, res, next) {
     let productController = require("../controllers/productControllers")
     productController
         .getProductById(req.params.productID)
-        .then(data =>
-        {
+        .then(data => {
             res.locals.product = data;
             return res.render('product-detail.ejs')
         })
 });
 
-router.post('/place-order', (req, res, next) =>
-{
+router.post('/place-order', (req, res, next) => {
     if (req.session.user == null)
         res.redirect('/user/login')
-    
+
     let date1 = new Date(req.body["start-date"]);
     let date2 = new Date(req.body["end-date"]);
     let diffDays = date2.getDate() - date1.getDate();
-    
+
     let order = {
         productId: req.body["productId"],
         totalPrice: req.body["price"] * req.body["quantity"] * diffDays,
@@ -58,13 +52,12 @@ router.post('/place-order', (req, res, next) =>
         end_date: req.body["end-date"],
         quantity: req.body["quantity"]
     }
-    
+
 
     res.render('place-order.ejs', { orderInfo: order })
 })
 
-router.post('/place-order/success', (req, res, next) =>
-{
+router.post('/place-order/success', (req, res, next) => {
     if (req.session.user == null)
         res.redirect('/user/login')
     let orderInfo = {
@@ -75,14 +68,14 @@ router.post('/place-order/success', (req, res, next) =>
         start_date: req.body["start-date"] + " 00:00:00",
         end_date: req.body["end-date"] + " 23:59:59",
         quantity: req.body["quantity"],
-        address: req.body["street"] + ", " + req.body["calc_shipping_district"] + ", "  + req.body["calc_shipping_provinces"],
+        address: req.body["street"] + ", " + req.body["calc_shipping_district"] + ", " + req.body["calc_shipping_provinces"],
         status: "Chờ xác nhận"
     }
 
     let orderController = require("../controllers/orderControllers")
     orderController
         .createOrder(orderInfo)
-        .then(() => res.send(orderInfo))
+        .then(() => res.render("announceSuccessfully.ejs", { message: "Đặt hàng thành công!" }))
 })
 
 module.exports = router;
